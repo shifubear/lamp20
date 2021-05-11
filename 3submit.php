7320660c78cdc50a2915d2d5d9654d64738eaa1f
yamaguti yuzuki <yamayuzuki0802@gmail.com>
2021-05-11 11:47:54 +0000

    参考画像追加

--- 3submit.php
+++ 3submit.php
@@ -9,7 +9,9 @@ if ($input == ""){
 
 }else{
 print "メッセージありがとうございます！ほかのボタンも遊んでみてください！";
-print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
+print "<br>";
+print "<br>";
+print '<img src="assets/img/step3/messagephp.png" class="img-fluid" alt="">';
 }
 
 
@@ -17,38 +19,37 @@ $yt = $_POST["youtube"];
 $tw = $_POST["twitter"];
 $ig = $_POST["instagram"];
 
-if ($yt == "on"){
-    print "YouTube";
-    print "<br>";
+if ($yt== "on" or $tw == "on" or $ig == "on"){
+    print '<img src="assets/img/step3/messagephp.png" class="img-fluid" alt="">';
+    if ($yt == "on"){
+        print "YouTube";
+        print "<br>";
+    }
+
+    if ($tw == "on"){
+        print "twitter";
+        print "<br>";
+    }
+
+    if ($ig == "on"){
+        print "instagram";
+        print "<br>";
+    }
 }
 
-if ($tw == "on"){
-    print "twitter";
-    print "<br>";
-}
-
-if ($ig == "on"){
-    print "instagram";
-    print "<br>";
-}
-print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
 
 $waza = $_POST["waza"];
  if ($waza == "10万ボルト"){
      print "10万ボルト";
-     print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
  } else if ($waza =="アイアンテール"){
      print "アイアンテール";
-     print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
  } else if ($waza == "ボルテッカー"){
      print "ボルテッカー";
-     print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
  }else if ($waza == "なきごえ"){
      print "なきごえ";
-     print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
  }
 
-
+ print '<a href="http://127.0.0.1:8080/lamp20/step3.html">戻る</a>';
 
 
 ?>
